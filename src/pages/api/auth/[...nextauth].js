31447f66772e5dc7b141fcdb833089dc448e65b9
tim <tschartman@yahoo.com>
2023-04-16 22:09:06 -0500

    debug

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -12,34 +12,34 @@ export const authOptions = {
       authorization: { params: { scope: 'user-modify-playback-state user-read-email user-read-private' } }
     })
   ],
-  callbacks: {
-    async session({session, token}) {
-      // session.accessToken = token.accessToken;
-      // session.refreshToken = token.refreshToken;
-      // session.expiresIn = token.expiresIn;
-      // return session;
-    },
-    async jwt({token, account}) {
-      // if (account && account.access_token) {
-      //   token.accessToken = account.access_token;
-      //   token.refreshToken = account.refresh_token;
-      //   token.expiresAt = account.expires_at * 1000;
-      // }
+  // callbacks: {
+  //   async session({session, token}) {
+  //     // session.accessToken = token.accessToken;
+  //     // session.refreshToken = token.refreshToken;
+  //     // session.expiresIn = token.expiresIn;
+  //     // return session;
+  //   },
+  //   async jwt({token, account}) {
+  //     // if (account && account.access_token) {
+  //     //   token.accessToken = account.access_token;
+  //     //   token.refreshToken = account.refresh_token;
+  //     //   token.expiresAt = account.expires_at * 1000;
+  //     // }
 
-      // // Return previous token if the access token has not expired yet
-      // if (Date.now() < token.expiresAt) {
-      //   return token;
-      // }
+  //     // // Return previous token if the access token has not expired yet
+  //     // if (Date.now() < token.expiresAt) {
+  //     //   return token;
+  //     // }
   
-      // // Access token has expired, try to update it
-      // const { accessToken, expiresIn } = await refreshAccessToken(token.refreshToken);
-      // return {
-      //   ...token,
-      //   accessToken,
-      //   expiresAt: Date.now() + expiresIn * 1000,
-      // };
-    }
-  }
+  //     // // Access token has expired, try to update it
+  //     // const { accessToken, expiresIn } = await refreshAccessToken(token.refreshToken);
+  //     // return {
+  //     //   ...token,
+  //     //   accessToken,
+  //     //   expiresAt: Date.now() + expiresIn * 1000,
+  //     // };
+  //   }
+  // }
 };
 
 
