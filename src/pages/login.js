2912917833055d32b249efee9e483003b485f948
tim <tschartman@yahoo.com>
2023-04-24 01:07:38 -0500

    push one more fix :)

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,20 @@
 import { signIn } from "next-auth/react";
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const Login = () => {
+  const router = useRouter();
+
+  const { data: session, status } = useSession();
+
+  if (session) {
+    router.replace('/');
+    return null;
+  }
+
   return (
     <div className="min-h-screen flex flex-col items-center pt-16 bg-stone-100">
-      <div className="bg-stone-200 p-8 w-2/3 rounded shadow-md">
+      <div className="bg-stone-200 p-8 w-80 rounded shadow-md">
         <h1 className="text-2xl font-bold mb-4 text-stone-800">Please log in</h1>
         <button
           onClick={() => signIn()}
